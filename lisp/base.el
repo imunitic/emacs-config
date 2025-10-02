13a8f3ce12a7f79a69e308fa3cd657f40a95b200
Ivica Munitić <imunitic@gmail.com>
2025-10-02 18:42:57 +0200

    [EMACS] :: added SPC f d keybind

--- lisp/base.el
+++ lisp/base.el
@@ -303,7 +303,8 @@ With C-u ARG, split horizontally (below)."
   (my/leader
     "f"   '(:ignore t :which-key "files")
     "ff"  '(find-file         :which-key "find file")
-    "fr"  '(recentf-open-files:which-key "recent files")
+    "fd"  '(dired       :which-key "find directory")
+    "fr"  '(recentf-open-files :which-key "recent files")
     "fs"  '(save-buffer       :which-key "save"))
 
   ;; ========== BUFFERS (SPC b) ==========
