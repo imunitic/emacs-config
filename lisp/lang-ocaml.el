24befd8d1a827c2377a98423f7f9332f1dcbe2fc
Ivica Munitić <imunitic@gmail.com>
2025-10-05 22:47:26 +0200

    [EMACS] :: small optimizations to lang-ocaml

--- lisp/lang-ocaml.el
+++ lisp/lang-ocaml.el
@@ -16,14 +16,17 @@
 ;; tuareg for older OCaml files
 (use-package tuareg)  ;; (no merlin hook)
 
-;; merlin installed but not auto-enabled (no hooks)
+;; merlin installed but not auto-enabled (ocamllsp provides LSP features)
 (use-package merlin
   :config
   (setq merlin-command "ocamlmerlin"))
 
-;; ocaml-lsp with eglot
+;; ocaml-lsp with eglot (auto-start)
 (defvar eglot-server-programs nil)  ;; avoid "void variable" on add-to-list
 (use-package eglot
+  :hook ((ocaml-ts-mode . eglot-ensure)
+         (tuareg-mode   . eglot-ensure)
+         (reason-mode   . eglot-ensure))
   :config
   (add-to-list 'eglot-server-programs '(tuareg-mode   . ("ocamllsp")))
   (add-to-list 'eglot-server-programs '(ocaml-ts-mode . ("ocamllsp")))
