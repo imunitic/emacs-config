a26f519f8b4b865d2a00aad098fd89da5c6ca9e8
Ivica Munitic <ivica.munitic@adcubum.com>
2025-12-11 11:47:09 +0100

    [EMACS] prefer built-in Org + relocate Org-roam DB

---
+++ lisp/tool-roam.el
@@ -0,0 +1,26 @@
+;;; tool-roam.el --- Org Roam -*- lexical-binding: t; -*-
+
+(use-package org-roam
+  :straight t
+  :custom
+  (org-roam-directory (file-truename "~/Development/documents/org/roam"))
+  (org-roam-db-location
+   (expand-file-name "org-roam.db"
+                     (file-truename "~/Development/documents/org/roam")))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(provide 'tool-roam)
+
+;;; tool-roam.el ends here
