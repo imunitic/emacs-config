5871bffe3b9a8ea498a4c34e03fa27c16910534c
Ivica Munitić <imunitic@gmail.com>
2026-03-02 08:14:52 +0100

    [EMACS :: org] TODO items

--- lisp/tool-org.el
+++ lisp/tool-org.el
@@ -8,6 +8,20 @@
   :config
   (require 'ox-md) ;; Markdown exporter bundled with built-in Org
   ;; Optional: toggle key for hiding images when needed
-  (global-set-key (kbd "C-c C-x C-v") #'org-toggle-inline-images))
+  (global-set-key (kbd "C-c C-x C-v") #'org-toggle-inline-images)
+
+  ;; TODO keyword sequence with shortcut keys
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "IN-PROGRESS(i)" "BLOCKED(b)" "REVIEW(r)"
+                    "|" "DONE(d)" "CANCELLED(c)")))
+
+  ;; Faces matched to ample-flat palette
+  (setq org-todo-keyword-faces
+        '(("TODO"        . (:foreground "#6C9EF8" :weight bold))
+          ("IN-PROGRESS" . (:foreground "#F0C674" :weight bold))
+          ("BLOCKED"     . (:foreground "#CC6666" :weight bold))
+          ("REVIEW"      . (:foreground "#B294BB" :weight bold))
+          ("DONE"        . (:foreground "#8ABA6A" :weight bold))
+          ("CANCELLED"   . (:foreground "#666666" :weight bold :strike-through t)))))
 
 (provide 'tool-org)
